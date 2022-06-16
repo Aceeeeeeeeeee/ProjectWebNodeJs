bffada2d78ca12b8ab24e6f828f94eaf5fff0fb0
Aceeeeeeeeeee <105403890+Aceeeeeeeeeee@users.noreply.github.com>
2022-06-16 14:59:59 +0700

    product

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const productRouter = express.Router();
+const productsRouter = require("./src/router/productsRouter.js")
 
 const app = express();
 const PORT = 3000;
@@ -13,21 +13,7 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-  res.render("products",{products:[
-    {productTitle: 'น้ำยาล้างจาน1', productDescription: 'สูตรใหม่1', productPrice: 40},
-    {productTitle: 'น้ำยาล้างจาน2', productDescription: 'สูตรใหม่2', productPrice: 42},
-    {productTitle: 'น้ำยาล้างจาน3', productDescription: 'สูตรใหม่3', productPrice: 45},
-    {productTitle: 'น้ำยาล้างจาน4', productDescription: 'สูตรใหม่4', productPrice: 50},
-  ]});
-});
-
-productRouter.route("/1").get((req, res) => {
-  res.set("Product 2 ");
-});
-
-app.use("/products", productRouter);
-
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", {
