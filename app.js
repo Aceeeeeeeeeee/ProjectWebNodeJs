efcae948ba2fccb3af2761f4ffcb5a22574939e3
Aceeeeeeeeeee <105403890+Aceeeeeeeeeee@users.noreply.github.com>
2022-06-16 12:02:24 +0700

    new

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug');
-const morgan = require('morgan');
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+const productRouter = express.Router();
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.use(morgan('combined'));
-app.get("/",(req,res) =>{res.send('Hello Taweechok Ace')});
-app.listen(port, () =>{debug("Listening on port %d" + chalk.red(" : " + port))});
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+productRouter.route("/").get((req, res) => {
+  res.set("Product 1 ");
+});
+
+productRouter.route("/1").get((req, res) => {
+  res.set("Product 2 ");
+});
+
+app.use("/Products", productRouter);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "TaweeChok",
+    customers: ["Tawee 1", "Tawee 2", "Tawee 3"],
+  });
+});
+app.listen(PORT, () => {
+  debug("Listening on port %d" + chalk.red(" : " + PORT));
+});
